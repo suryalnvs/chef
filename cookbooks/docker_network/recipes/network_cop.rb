bede71cafbb74673601f10b5daa8c4a62879fb08
suryalnvs <suryalnvs@gmail.com>
2017-01-03 20:05:38 +0000

    tested succesfully for cop and orderer

--- cookbooks/docker_network/recipes/network_cop.rb
+++ cookbooks/docker_network/recipes/network_cop.rb
@@ -11,17 +11,17 @@ end
 docker_container 'cop1' do
    repo 'lhaskins/fabric-cop'
    tag 'latest'
-   command "sh -c 'cop server start -ca ~/.cop/ec.pem -ca-key ~/.cop/ec-key.pem -config /config/cop.json -address 0.0.0.0'"
+   command 'sh -c "cop server start -ca ~/.cop/ec.pem -ca-key ~/.cop/ec-key.pem -config /config/cop.json -address 0.0.0.0"'
    port '8888:8888'
    not_if "[ ! -z `docker ps -qaf 'name=cop1'` ]"
    action :run
 end
 
 docker_container 'orderer1' do
-   repo 'lhaskins/fabbric-orderer'
+   repo 'lhaskins/fabric-orderer'
    tag 'latest'
    command 'orderer'
-   env ['"ORDERER_GENERAL_LEDGERTYPE": "ram"', '"ORDERER_GENERAL_BATCHTIMEOUT": "10s"', '"ORDERER_GENERAL_BATCHSIZE": "10"', '"ORDERER_GENERAL_MAXWINDOWSIZE": "1000"', '"ORDERER_GENERAL_ORDERERTYPE": "solo"', '"ORDERER_GENERAL_LISTENADDRESS": "0.0.0.0"', '"ORDERER_RAMLEDGER_HISTORY_SIZE": "100"']
+   env [ '"ORDERER_GENERAL_LEDGERTYPE": "ram"', '"ORDERER_GENERAL_BATCHTIMEOUT": "10s"', '"ORDERER_GENERAL_BATCHSIZE": "10"', '"ORDERER_GENERAL_MAXWINDOWSIZE": "1000"', '"ORDERER_GENERAL_ORDERERTYPE": "solo"', '"ORDERER_GENERAL_LISTENADDRESS": "0.0.0.0"', '"ORDERER_RAMLEDGER_HISTORY_SIZE": "100"' ]
    port '5151:7050'
    not_if "[ ! -z `docker ps -qaf 'name=orderer1'` ]"
    action :run
